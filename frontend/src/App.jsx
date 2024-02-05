7726dd8d3f92e2b722f8be6eb157d3b4f0e582a2
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2024-02-05 10:45:17 +0500

    signup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,30 @@ import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { useAuth } from './context/AuthContext';
 
 export const BASE_URL = 'http://localhost:8000';
 
 function App() {
+  const { authUser } = useAuth();
+
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
+        <Route
+          path='/'
+          element={authUser ? <Home /> : <Navigate to='/login' />}
+        />
+        <Route
+          path='/login'
+          element={authUser ? <Navigate to='/' /> : <Login />}
+        />
+        <Route
+          path='/signup'
+          element={authUser ? <Navigate to='/' /> : <Signup />}
+        />
       </Routes>
       <Toaster />
     </div>
--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { BASE_URL } from '../App';
+import { useAuth } from '../context/AuthContext';
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuth();
 
   const signup = async ({
     fullName,
@@ -32,6 +34,10 @@ const useSignup = () => {
       if (!res.ok) {
         return toast.error(data.message);
       }
+
+      // If the user is successfully signed up, redirect to the login page and display a success message to the user and clear the form fields. store user in local storage
+      localStorage.setItem('authUser', JSON.stringify(data.user));
+      setAuthUser(data.user);
 
       toast.success(data.message);
     } catch (error) {
