6e057a3cd4beeaf1ae48022d9a265ade1230c832
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2024-02-05 11:40:12 +0500

    login

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { useAuth } from '../context/AuthContext';
+import { BASE_URL } from '../App';
+import { toast } from 'react-hot-toast';
+
+const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuth();
+
+  const signin = async ({ username, password }) => {
+    try {
+      setLoading(true);
+      const res = await fetch(`${BASE_URL}/api/auth/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        return toast.error(data.message);
+      }
+
+      localStorage.setItem('authUser', JSON.stringify(data.user));
+      setAuthUser(data.user);
+
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return [loading, signin];
+};
+
+export default useLogin;
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -4,20 +4,40 @@ import Input from '../../components/Input';
 import BlurryContainer from '../../components/BlurryContainer';
 import Container from '../../components/Container';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import useLogin from '../../hooks/useLogin';
 
 const Login = () => {
+  const [laoding, signin] = useLogin();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  });
+
+  const submitHandler = async (data) => {
+    await signin(data);
+  };
+
   return (
     <Container>
       <BlurryContainer>
         <BrandHeading span1={'Login'} span2={'ChatApp'} />
         {/* FORM */}
-        <form>
+        <form onSubmit={handleSubmit(submitHandler)}>
           {/* USERNAME FIELD */}
           <Input
             type={'text'}
             id={'username'}
             label={'Username'}
             placeHolder={'Enter username'}
+            register={register}
+            errors={errors}
           />
 
           {/* PASSWORD FIELD */}
@@ -26,6 +46,8 @@ const Login = () => {
             id={'password'}
             label={'Password'}
             placeHolder={'Enter password'}
+            register={register}
+            errors={errors}
           />
 
           {/* DON'T HAVE AN ACCOUNT */}
@@ -37,7 +59,7 @@ const Login = () => {
           </Link>
 
           {/* SUBMIT BUTTON */}
-          <Button>Login</Button>
+          <Button disabled={laoding}>Login</Button>
         </form>
       </BlurryContainer>
     </Container>
