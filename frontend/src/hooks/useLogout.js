5dd325d656f10ee0a26b848cae0afe334678c174
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2024-02-05 10:56:37 +0500

    logout done

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { useAuth } from '../context/AuthContext';
+import { toast } from 'react-hot-toast';
+import { BASE_URL } from '../App';
+
+const useLogout = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuth();
+
+  const logout = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(`${BASE_URL}/api/auth/logout`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!res.ok) {
+        throw new Error('Something went wrong');
+      }
+
+      // remove user from local storage
+      localStorage.removeItem('authUser');
+      // remove user from context
+      setAuthUser(null);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, logout };
+};
+
+export default useLogout;
