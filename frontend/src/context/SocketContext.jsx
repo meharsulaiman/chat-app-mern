ced283d1e3309e72b5e9ad20ce1a7a693229f6c4
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2024-02-06 14:48:41 +0500

    add server url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -11,7 +11,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io('http://localhost:8000', {
+      const socket = io('https://chat-mern-app-sulaiman.onrender.com', {
         query: {
           userId: authUser.id,
         },
