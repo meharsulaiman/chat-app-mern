b8ece68a2f0e1e969df1439115c182df2dd7c724
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2024-02-06 08:41:30 +0500

    search conversations

--- frontend/src/components/SearchInput.jsx
+++ frontend/src/components/SearchInput.jsx
@@ -1,9 +1,41 @@
+import { useState } from 'react';
+import { toast } from 'react-hot-toast';
 import { IoSearchSharp } from 'react-icons/io5';
+import useConversation from '../store/useConversation';
+import useGetConversations from '../hooks/useGetConversations';
 
 const SearchInput = () => {
+  const [search, setSearch] = useState('');
+
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    if (search.length < 3) {
+      return toast.error('Search query must be at least 3 characters long');
+    }
+
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (!conversation) {
+      return toast.error('No conversation found');
+    }
+
+    setSelectedConversation(conversation);
+    setSearch('');
+  };
+
   return (
-    <form className='flex items-center gap-2'>
+    <form className='flex items-center gap-2' onSubmit={handleSubmit}>
       <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
         type='text'
         placeholder='Search...'
         className='input input-bordered rounded-full'
